d92012cca9fe7bc36cc7d8cfd6d1f78e7263c722
Samuele Chiocca <samuele@sighup.io>
2021-11-08 16:20:40 +0100

    Adapt plugin for deck

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for deck.
 GH_REPO="https://github.com/Kong/deck"
 TOOL_NAME="deck"
 TOOL_TEST="deck --help"
@@ -14,7 +13,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if deck is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -27,22 +25,43 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//'
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if deck has other means of determining installable versions.
   list_github_tags
 }
 
+get_platform() {
+  local platform
+  platform=$(uname)
+  case $platform in
+  Darwin) platform="macOS" ;;
+  Linux) platform="Linux" ;;
+  FreeBSD) platform="FreeBSD" ;;
+  Windows) platform="Windows" ;;
+  esac
+  echo "$platform"
+}
+
+get_system_architecture() {
+  local architecture
+  architecture=$(uname -m)
+  case $architecture in
+  armv*) architecture="ARM" ;;
+  aarch64) architecture="ARM64" ;;
+  x86_64) architecture="64bit" ;;
+  esac
+  echo "$architecture"
+}
 download_release() {
-  local version filename url
+  local version platform architecture filename url
   version="$1"
+  platform="$(get_platform)"
+  architecture="$(get_system_architecture)"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for deck
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="$GH_REPO/releases/download/v${version}/deck_${version}_${platform}_${architecture}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +80,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Asert deck executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
